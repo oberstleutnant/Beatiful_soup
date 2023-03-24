622cb4d968c2580e48bf3b42e3acff9df65ffe34
Emre <69721351+oberstleutnant@users.noreply.github.com>
2023-03-24 22:30:00 +0300

    Update CukurovaUni.py

--- CukurovaUni.py
+++ CukurovaUni.py
@@ -1,4 +1,57 @@
-import requests
+import requestsimport requests
+from bs4 import BeautifulSoup
+
+url = "https://www.erbakan.edu.tr/biyoteknoloji/anabilim"
+
+html = requests.get(url).content
+soup = BeautifulSoup(html,"html.parser")
+
+list= soup.find_all("div",{"class":"col-xs-12 col-sm-6 col-md-4 academic-person"})
+
+
+for i in list:
+    isim =i.find("div",{"class":"academic-person-name"}).find_all("span")
+
+for a in list:
+    mail=a.find("a",{"class":"academic-person-mail"},href=True)
+    emails=print(mail["href"])
+
+print("-------------------------------------------")
+print("-------------------------------------------")
+print("-------------------------------------------")
+
+
+isim = BeautifulSoup(html,"html.parser")
+mail = BeautifulSoup(html,"html.parser")
+
+
+
+for isim_tag in isim.find_all('span'):
+    print(isim_tag.text)
+
+
+
+""" for mail_tag in mail.find_all('href'):
+    print(mail_tag.text) """
+
+
+print("-------------------------------------------")
+print("-------------------------------------------")
+print("-------------------------------------------")
+    
+
+""" def decodeEmail(e):
+    de = ""
+    k = int(e[:2], 16)
+
+    for i in range(2, len(e)-1, 2):
+        de += chr(int(e[i:i+2], 16)^k)
+
+    return de
+
+de_emails = decodeEmail(emails)
+print(de_emails) """
+
 from bs4 import BeautifulSoup
 
 url = "https://bmb.cu.edu.tr/cu/personel/akademik-personel"
